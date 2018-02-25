1625efd1ea61abeaeea32d8af92f1215c03e54b9
Lukasz Matusik <kontakt@piwolucja.pl>
2018-02-25 20:44:08 +0100

    Dodalem zmiane

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,6 +67,7 @@
     <body>
         <div class="flex-center">
             <div class="content">
+                <!-- Error handling  -->
                 @if ($errors_count > 0)
                 <h3>Błędy formularza</h3>
                     @foreach ($errors AS $error)
