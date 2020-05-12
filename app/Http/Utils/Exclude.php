6e2d9d72fe24c0ea6064ac5a8f94ee98d6c68d94
Łukasz Matusik <lukasz.matusik@schibsted.com>
2020-05-12 14:49:43 +0200

    Trailing commas in arrays

--- app/Http/Utils/Exclude.php
+++ app/Http/Utils/Exclude.php
@@ -43,17 +43,17 @@ class Exclude
 
         // wykluczenia dla piw lekkich
         if ( isset( $answerValue[3] ) && $answerValue[3] === 'coś lekkiego' ) {
-            $userOptions->excludeFromRecommended( [ 7, 8, 22, 36, 37, 38, 39, 50, 67, 998, 999 ] );
+            $userOptions->excludeFromRecommended( [ 7, 8, 22, 36, 37, 38, 39, 50, 67, 998, 999, ] );
         }
 
         // wykluczenia dla piw ciemnych
         if ( isset( $answerValue[5] ) && $answerValue[5] === 'ciemne' ) {
-            $userOptions->excludeFromRecommended( [ 999 ] );
+            $userOptions->excludeFromRecommended( [ 999, ] );
         }
 
         // wykluczenia dla piw jasnych
         if ( isset( $answerValue[5] ) && $answerValue[5] === 'jasne' ) {
-            $userOptions->excludeFromRecommended( [ 3, 21, 33, 34, 35, 36, 37, 71, 74, 998 ] );
+            $userOptions->excludeFromRecommended( [ 3, 21, 33, 34, 35, 36, 37, 71, 74, 998, ] );
         }
     }
 }
