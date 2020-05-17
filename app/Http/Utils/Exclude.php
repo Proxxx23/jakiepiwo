6f90641082adc060b4827e646ad572c88ed9e0b8
Lukasz Matusik <kontakt@piwolucja.pl>
2020-05-17 20:44:19 +0200

    Unsuitable exclusions for pale/dark

--- app/Http/Utils/Exclude.php
+++ app/Http/Utils/Exclude.php
@@ -49,11 +49,13 @@ class Exclude
         // wykluczenia dla piw jasnych
         if ( isset( $answerValue[5] ) && $answerValue[5] === 'ciemne' ) {
             $userOptions->excludeFromRecommended( [ 1, 2, 6, 9, 10, 11, 13, 14, 25, 40, 44, 45, 49, 51, 52, 60, 61, 68, 69, 70, 72, 999, ] );
+            $userOptions->excludeFromUnsuitable( [ 3, 21, 33, 34, 35, 36, 37, 71, 74, 998, ] );
         }
 
         // wykluczenia dla piw ciemnych
         if ( isset( $answerValue[5] ) && $answerValue[5] === 'jasne' ) {
             $userOptions->excludeFromRecommended( [ 3, 21, 33, 34, 35, 36, 37, 71, 74, 998, ] );
+            $userOptions->excludeFromUnsuitable( [ 1, 2, 6, 9, 10, 11, 13, 14, 25, 40, 44, 45, 49, 51, 52, 60, 61, 68, 69, 70, 72, 999, ] );
         }
     }
 }
